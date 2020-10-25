d757c688135cc618d75e741db240c3381c7d6ef0
MariaPSegarra <mariapsegarra@gmail.com>
2020-10-25 16:17:58 +0000

    Add login page and functionality

--- app.py
+++ app.py
@@ -32,21 +32,71 @@ def register():
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
+        existing_email = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
 
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
+        if existing_email:
+            flash("Email already exists")
+            return redirect(url_for("register"))
 
         register = {
             "username": request.form.get("username").lower(),
+            "email": request.form.get("email").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
-        flash("Welcome to the Club!")
+        flash("Registration Successful!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        existing_email = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower(),
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Log In. Please Try Again.")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Log In. Please Try Again.")
+            return redirect(url_for("login"))
+        
+        if existing_email:
+            # ensure hashed password matches email input
+            if check_password_hash(
+                existing_email["password"], request.form.get("password")):
+                    session["email"] = request.form.get("email").lower(),
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Log In. Please Try Again.")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Log In. <br> Please Try Again.")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
